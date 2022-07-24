20f0f007548275301eb1cdb55aed40ef1e1e5c92
Alpen Prastoyo <alpenprastoyo@gmail.com>
2022-07-24 17:25:49 +0000

    first upload

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -1,8 +1,20 @@
 package controller
 
+import "github.com/gin-gonic/gin"
 
 //AuthController interface is a contract what this controller can do
-type AuthController inteface {
+type AuthController interface {
 	Login(ctx *gin.Context)
 	Register(ctx *gin.Context)
 }
+
+type authController struct {
+	//di sini masukkan service yang kalian butuh
+	//this is where you put your service
+}
+
+func NewAuthController() AuthController {
+	return &authController{}
+}
+
+func (c *authController) Login(ct)
